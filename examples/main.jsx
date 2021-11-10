2f67d7c46c7182cd9195ae74f4e5f5ba17c714b0
dark <502342826@qq.com>
2021-11-10 11:22:11 +0800

    chore: 优化

--- examples/main.jsx
+++ examples/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import ReactDarkPhoto from '../packages/react-dark-photo/index.jsx'
+import Wave from "../packages/react-dark-photo/components/wave/app";
 
 const imgData = "https://dark2017.github.io/vue-dark-photo.github.io/img/logo.82b9c7a5.png"
 const imgData1 = "https://camo.githubusercontent.com/6c28f1291fb0e2b404c4fd8393298c8602c62d7f3f862404dedbfc862d466c27/68747470733a2f2f6769746875622d726561646d652d73746174732e616e7572616768617a7261312e76657263656c2e6170702f6170693f757365726e616d653d4461726b323031372673686f775f69636f6e733d7472756526696e636c7564655f616c6c5f636f6d6d6974733d74727565267468656d653d7261646963616c"
@@ -31,7 +32,7 @@ class App extends React.Component{
   render() {
     const { showBox, imgArr } = this.state
     return (
-      <div>
+      <div style={{ height: '1100px' }}>
         <button onClick={this.open}>open</button>
         <ReactDarkPhoto 
           showBox={showBox}
@@ -42,6 +43,7 @@ class App extends React.Component{
           isHint={true}
           ifWave={true}
         />
+        <Wave/>
       </div>
     )
   }
